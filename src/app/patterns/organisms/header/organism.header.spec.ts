b1e7d61a3285d9f851cdc415bcd650b69ef1b10c
Henry Zeitler <h.zeitler@webartig.com>
2018-01-17 15:29:59 +0100

    adjust karma test

--- src/app/patterns/organisms/header/organism.header.spec.ts
+++ src/app/patterns/organisms/header/organism.header.spec.ts
@@ -1,13 +1,20 @@
 import { TestBed, async } from '@angular/core/testing';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import {HttpModule} from '@angular/http';
 import { OrganismHeader } from './organism.header';
+import { AtomButton } from '../../atoms/button/atom.button';
 
-describe('AppComponent', () => {
+
+describe('OrganismHeader', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        OrganismHeader
+        OrganismHeader,
+        AtomButton
       ],
+      imports:[HttpClientModule, HttpModule],
+      providers:[HttpClientModule]
     }).compileComponents();
   }));
 
@@ -17,17 +24,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Playground'`, async(() => {
     const fixture = TestBed.createComponent(OrganismHeader);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Playground');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(OrganismHeader);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Playground');
   }));
 
 });
